dfaaf9c56b98ff8103b1564b296fd174c54fdbef
mvgmb <mv3gmb@gmail.com>
2021-04-05 23:00:08 -0300

    init

---
+++ app/src/main/java/com/cin/animalrescue/ui/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cin.animalrescue.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cin.animalrescue.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
