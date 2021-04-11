da5ffc63aef53f1c95cbf56384621a9ab571e814
mvgmb <mv3gmb@gmail.com>
2021-04-11 16:33:12 -0300

    feat(animal_add): add registerAnimalButton handler

--- app/src/main/java/com/cin/animalrescue/ui/component/animal_add/view/AnimalAddActivity.kt
+++ app/src/main/java/com/cin/animalrescue/ui/component/animal_add/view/AnimalAddActivity.kt
@@ -4,8 +4,8 @@ import androidx.activity.viewModels
 import com.cin.animalrescue.data.model.Animal
 import com.cin.animalrescue.databinding.ActivityAnimalAddBinding
 import com.cin.animalrescue.ui.component.animal_add.viewmodel.AnimalAddViewModel
-import com.cin.animalrescue.ui.component.animal_list.viewmodel.AnimalListViewModel
 import com.task.ui.base.BaseActivity
+import java.util.*
 
 class AnimalAddActivity : BaseActivity() {
     private lateinit var binding: ActivityAnimalAddBinding
@@ -16,19 +16,36 @@ class AnimalAddActivity : BaseActivity() {
         binding = ActivityAnimalAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnRegister.setOnClickListener { handleBtnRegisterClick() }
+
         binding.btnTest.setOnClickListener {
-            i += 1
+            val id = UUID.randomUUID().toString()
+            val smallID = id.substring(0, 5)
             animalListViewModel.insert(
                 Animal(
-                    "id_$i",
-                    "helper_$i",
-                    "type_$i",
-                    "title_$i",
-                    "location_$i",
-                    "info_$i"
+                    id = id,
+                    "helper_$smallID",
+                    "type_$smallID",
+                    "title_$smallID",
+                    "location_$smallID",
+                    "info_$smallID"
                 )
             )
         }
+    }
+
+    private fun handleBtnRegisterClick() {
+        animalListViewModel.insert(
+            Animal(
+                id = UUID.randomUUID().toString(),
+                helper = "current_user",
+                type = binding.type.text.toString(),
+                title = binding.title.text.toString(),
+                location = binding.location.text.toString(),
+                info = binding.info.text.toString(),
+            )
+        )
+        finish()
     }
 
     override fun observeViewModel() {}
