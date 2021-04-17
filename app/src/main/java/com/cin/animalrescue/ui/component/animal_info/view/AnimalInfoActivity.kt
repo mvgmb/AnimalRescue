8bd7da31db2392d34bfc52981cfbb25b8383b72c
mvgmb <mv3gmb@gmail.com>
2021-04-17 16:06:16 -0300

    feat: add init MapActivity

--- app/src/main/java/com/cin/animalrescue/ui/component/animal_info/view/AnimalInfoActivity.kt
+++ app/src/main/java/com/cin/animalrescue/ui/component/animal_info/view/AnimalInfoActivity.kt
@@ -1,10 +1,11 @@
 package com.cin.animalrescue.ui.component.animal_info.view
 
+import android.content.Intent
 import androidx.activity.viewModels
 import com.cin.animalrescue.data.model.Animal
 import com.cin.animalrescue.databinding.ActivityAnimalInfoBinding
-import com.cin.animalrescue.ui.component.animal_add.viewmodel.AnimalAddViewModel
 import com.cin.animalrescue.ui.component.animal_info.viewmodel.AnimalInfoViewModel
+import com.cin.animalrescue.ui.component.map.view.MapsActivity
 import com.cin.animalrescue.utils.observe
 import com.task.ui.base.BaseActivity
 
@@ -13,12 +14,19 @@ class AnimalInfoActivity : BaseActivity() {
 
     private lateinit var binding: ActivityAnimalInfoBinding
     private lateinit var id: String
+    private lateinit var location: String
 
     override fun initViewBinding() {
         binding = ActivityAnimalInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         id = intent.getStringExtra("id").toString()
+
+        binding.btnMaps.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+                .putExtra("location", location)
+            startActivity(intent)
+        }
     }
 
     override fun observeViewModel() {
@@ -30,6 +38,7 @@ class AnimalInfoActivity : BaseActivity() {
         binding.helper.text = animal.helper
         binding.type.text = animal.type
         binding.info.text = animal.info
-        binding.location.text = animal.location
+        location = animal.location
+        binding.location.text = location
     }
 }
