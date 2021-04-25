0282128bff0bae3b94d56ef7e560da7ac7e016d4
mvgmb <mv3gmb@gmail.com>
2021-04-25 17:18:30 -0300

    feat: add initial download from firebase storage logic

--- app/src/main/java/com/cin/animalrescue/ui/component/animal_info/AnimalInfoActivity.kt
+++ app/src/main/java/com/cin/animalrescue/ui/component/animal_info/AnimalInfoActivity.kt
@@ -1,12 +1,18 @@
 package com.cin.animalrescue.ui.component.animal_info
 
 import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
 import com.cin.animalrescue.data.model.Animal
 import com.cin.animalrescue.databinding.ActivityAnimalInfoBinding
 import com.cin.animalrescue.ui.component.map.MapsActivity
 import com.cin.animalrescue.utils.observe
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.task.ui.base.BaseActivity
+import java.io.File
 
 class AnimalInfoActivity : BaseActivity() {
     private val animalInfoViewModel: AnimalInfoViewModel by viewModels()
@@ -37,6 +43,14 @@ class AnimalInfoActivity : BaseActivity() {
     }
 
     private fun handleGetAnimalById(animal: Animal) {
+        val storageReference = Firebase.storage.reference.child("images/${animal.id}")
+        val localFile = File.createTempFile(animal.id, "jpg")
+        storageReference.getFile(localFile).addOnSuccessListener {
+            binding.imageView.setImageURI(Uri.fromFile(localFile))
+        }.addOnFailureListener { e ->
+            Log.e("MY_TAG", e.toString())
+        }
+
         binding.title.text = animal.title
         binding.helperUid.text = animal.helper_uid
         binding.type.text = animal.type
